51db7bfacf6402b00f6fc74cc8401230fda1f99c
Mariana Santos <maririb51@gmail.com>
2023-11-08 10:25:15 +0000

    getMe deve retornar userDTO quando client está logado.

--- src/test/java/com/devsuperior/dscommerce/controllers/IT/UserControllerIT.java
+++ src/test/java/com/devsuperior/dscommerce/controllers/IT/UserControllerIT.java
@@ -25,16 +25,19 @@ public class UserControllerIT {
 	@Autowired
 	private TokenUtil tokenUtil;
 	
-	private String adminUsername, adminPassword;
-	private String adminToken;
+	private String adminUsername, adminPassword, clientUsername, clientPassword;
+	private String adminToken, clientToken;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		
 		adminUsername = "alex@gmail.com";
 		adminPassword = "123456";
+		clientUsername = "maria@gmail.com";
+		clientPassword = "123456";
 		
 		adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+		clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
 
 	}
 
@@ -52,6 +55,22 @@ public void getMeShouldReturnUserDTOWhenAdminLogged() throws Exception {
 	result.andExpect(jsonPath("$.email").value("alex@gmail.com"));
 	result.andExpect(jsonPath("$.phone").value("977777777"));	
 	result.andExpect(jsonPath("$.birthDate").value("1987-12-13"));		
+	result.andExpect(jsonPath("$.roles").exists());
+ }
+@Test
+public void getMeShouldReturnUserDTOWhenClientLogged() throws Exception {
+	
+	ResultActions result = 
+			mockMvc.perform(get("/users/me")
+				.header("Authorization", "Bearer " + clientToken)
+				.accept(MediaType.APPLICATION_JSON));
+	
+	result.andExpect(status().isOk());
+	result.andExpect(jsonPath("$.id").value(1L));
+	result.andExpect(jsonPath("$.name").value("Maria Brown"));
+	result.andExpect(jsonPath("$.email").value("maria@gmail.com"));
+	result.andExpect(jsonPath("$.phone").value("988888888"));	
+	result.andExpect(jsonPath("$.birthDate").value("2001-07-25"));		
 	result.andExpect(jsonPath("$.roles").exists());
  }
 }
